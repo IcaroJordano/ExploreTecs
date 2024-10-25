957268311c95eea4cdba798f7af7b562c9bee317
Icaro Jordano <Icarojordano>
2024-10-25 05:04:52 -0300

    feat: requisicao cms and render produtos

--- src/components/ExibirProdutos.jsx
+++ src/components/ExibirProdutos.jsx
@@ -1,13 +1,48 @@
+import { useEffect } from "react";
 import { CardProduto } from "./CardProduto";
+import { useState } from "react";
 
 export function ExibirProdutos() {
+    const [produtos,setProdutos]=useState([])
+    useEffect(()=>{
+        fetch(
+            'https://graphql.datocms.com/',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Icaro Jordano ${'d26af1aed529c01033f2abecbcd798'}`,
+              },
+              body: JSON.stringify({
+                query: `{
+              allProdutos{
+                linkImage
+                name
+                preco
+}
+  }`
+              }),
+            }
+          )
+          .then(res => res.json())
+          .then((res) => {
+            console.log(res.data)
+            setProdutos(res.data['allProdutos'])
+            console.log(produtos[0])
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    },[])
     return (
         <section className=" h-screen bg-neutral-200 pt-10">
-            <div className="w-full flex-wrap mx-auto flex justify-center ">
-                <CardProduto></CardProduto>
-                <CardProduto></CardProduto>
-                <CardProduto></CardProduto>
-                <CardProduto></CardProduto>
+            <div className="w-full flex-wrap  mx-auto flex justify-center ">
+                {/* <p>{produtos}</p> */}
+                {produtos.map((item)=>(
+                    <CardProduto nome={item.name} preco={item.preco} image={item.linkImage} ></CardProduto>
+                ))
+                }
             </div>
         </section>
     )
