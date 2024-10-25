6660af5706564bbaf301d435cafdb61ae6c12565
Icaro Jordano <Icarojordano>
2024-10-25 06:47:14 -0300

    feat: version request com axios

--- src/components/ExibirProdutos.jsx
+++ src/components/ExibirProdutos.jsx
@@ -2,33 +2,33 @@ import { useEffect } from "react";
 import { CardProduto } from "./CardProduto";
 import { useState } from "react";
 
+import axios from "axios";
+
 export function ExibirProdutos() {
     const [produtos,setProdutos]=useState([])
     useEffect(()=>{
-        fetch(
+        axios.post(
             'https://graphql.datocms.com/',
             {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Authorization': `Icaro Jordano ${'d26af1aed529c01033f2abecbcd798'}`,
-              },
-              body: JSON.stringify({
                 query: `{
-              allProdutos{
-                linkImage
-                name
-                preco
-}
-  }`
-              }),
-            }
-          )
-          .then(res => res.json())
+                    allProdutos{
+                        linkImage
+                        name
+                        preco
+                    }
+                }`
+            },
+            {
+                headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': `Icaro Jordano d26af1aed529c01033f2abecbcd798`,
+          }
+    }
+)
           .then((res) => {
             console.log(res.data)
-            setProdutos(res.data['allProdutos'])
+            setProdutos(res.data.data['allProdutos'])
             console.log(produtos[0])
           })
           .catch((error) => {
@@ -38,7 +38,6 @@ export function ExibirProdutos() {
     return (
         <section className=" h-screen bg-white pt-10">
             <div className="w-full  flex-wrap  mx-auto flex lg:justify-normal justify-center ">
-                {/* <p>{produtos}</p> */}
                 {produtos.map((item)=>(
                     <CardProduto nome={item.name} preco={item.preco} image={item.linkImage} ></CardProduto>
                 ))
